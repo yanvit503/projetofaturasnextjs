019d9d32868286c6b133a28c82edf0da158c85b6
Yan Vitor <yanvit503@gmail.com>
2023-11-11 16:50:01 -0300

    Commit Inicial

---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import { Inter } from 'next/font/google'
+import './globals.css'
+import NavBar from './Components/NavBar'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <div className='flex'>
+          <NavBar />
+          <div>
+            {children}
+          </div>
+        </div>
+      </body>
+    </html>
+  )
+}
